134bdd4d54d001502995f5957b26a065e873c7be
BenCamua <37817613+BenCamua@users.noreply.github.com>
2018-03-27 09:40:19 +1100

    Add files via upload

--- GameIdeaTests.py
+++ GameIdeaTests.py
@@ -1,5 +1,10 @@
 import sys
 
+def gameStart(): 
+    UserName = input('Hello young traveller may I ask what is your name: ')
+    print('So your name is ' + UserName + '? What a wonderful name for our next lengendary warrior!')
+
+    
 def WarriorQuestion():
     validInput = False
     while validInput == False:
@@ -7,7 +12,6 @@ def WarriorQuestion():
         if Join.startswith('y'):
            print("Great, welcome to the game called Eindhoven " + UserName + '!')
            validInput = True
-           gameLoop()
         elif Join.startswith('n'):
               print ("Sorry for asking, comeback when you are ready.")
               sys.exit()
@@ -16,13 +20,12 @@ def WarriorQuestion():
               validInput = False
         
 
-def gameLoop():
-    print('yrdgfdg')
+def Chooseclass():
+    UserClass = input(' 
     
 
-UserName = input('Hello young traveller may I ask what is your name: ')
-print('So your name is ' + UserName + '? What a wonderful name for our next lengendary warrior!')
-
+gameStart()
 WarriorQuestion()
+Chooseclass()
 
 
